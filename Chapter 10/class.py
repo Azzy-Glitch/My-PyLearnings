3a4431210e414b9cab31bf1005380a67d1df4aa6
Abdul Aziz <aapppp742@gmail.com>
2025-09-11 12:34:29 +0500

    Create class.py

---
+++ Chapter 10/class.py
@@ -0,0 +1,12 @@
+class Employee:
+    position = "Software Engineer"
+    salary = 70000
+    department = "IT"
+
+Azzy = Employee()
+Azzy.name = "Azzy"
+print(Azzy.name, Azzy.position, Azzy.salary, Azzy.department)
+
+John = Employee()
+John.name = "John"
+print(John.name, John.position, John.salary, John.department)
